97e86cc364ff161ffd575e3130932df1460fff18
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-04-08 20:32:38 +0300

    Add YouTube API key

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import replace from "@rollup/plugin-replace";
+import dotenv from "dotenv";
 
-// https://vitejs.dev/config/
+// Load variables from .env file
+dotenv.config();
+
+const apiKey = process.env.API_KEY;
+
+if (!apiKey) {
+  throw new Error("API_KEY is missing in the environment variables. Please set it in your .env file.");
+}
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    replace({
+      __API_KEY__: JSON.stringify(apiKey),
+    }),
+  ],
+});
