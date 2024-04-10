89bd42f5e422dbdfd317d81b9a7485d661b83f11
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-04-10 22:41:52 +0300

    Display message if no search results in SongSearchBar component

--- src/cmps/SongSearchBar/SongSearchBar.jsx
+++ src/cmps/SongSearchBar/SongSearchBar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 import { SearchIcon } from "../icons/SearchIcon";
@@ -7,6 +7,12 @@ import { utilService } from "../../services/util.service";
 
 export function SongSearchBar() {
   const [songs, setSongs] = useState([]);
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const maxResults = 5;
+    debounceSearch(query, maxResults);
+  }, [query]);
 
   /**
    * Searches for videos on YouTube based on the provided query string.
@@ -15,49 +21,42 @@ export function SongSearchBar() {
    * @returns {Promise<Array<{title: string, videoId: string}>>} - A Promise that resolves with an array of search results, each containing title and videoId.
    */
   async function searchVideos(query, maxResults) {
-    // Base URL for YouTube Data API
-    const base_url = "https://www.googleapis.com/youtube/v3/search";
-
-    try {
-      // Define parameters for the request
-      const params = {
-        key: __API_KEY__,
-        part: "snippet", // Specifies the snippet resource properties that the API response will include
-        q: query, // The search query string
-        type: "video", // Restricts the search results to only include videos
-        maxResults: maxResults, // Maximum number of results to return
-      };
-
-      // Make the request to the API using Axios and await the response
-      const response = await axios.get(base_url, { params });
-
-      // Extract information from the response
-      const results = response.data.items.map((item) => ({
-        title: item.snippet.title,
-        id: item.id.videoId,
-        imgUrl: item.snippet.thumbnails.medium.url,
-      }));
-
-      return results;
-    } catch (error) {
-      console.error("Error:", error);
-      return []; // Return an empty array if there's an error
-    }
-  }
-
-  async function onChange(ev) {
-    const query = ev.target.value;
     if (query) {
-      const searchResult = await searchVideos(query, 5); // Get top 5 results
-      if (searchResult) {
-        setSongs(searchResult);
-      } else {
-        setSongs(null); // No results found
+      // Base URL for YouTube Data API
+      const base_url = "https://www.googleapis.com/youtube/v3/search";
+
+      try {
+        // Define parameters for the request
+        const params = {
+          key: __API_KEY__,
+          part: "snippet", // Specifies the snippet resource properties that the API response will include
+          q: query, // The search query string
+          type: "video", // Restricts the search results to only include videos
+          maxResults: maxResults, // Maximum number of results to return
+        };
+
+        // Make the request to the API using Axios and await the response
+        const response = await axios.get(base_url, { params });
+
+        // Extract information from the response
+        const results = response.data.items.map((item) => ({
+          title: item.snippet.title,
+          id: item.id.videoId,
+          imgUrl: item.snippet.thumbnails.medium.url,
+        }));
+
+        return results;
+      } catch (error) {
+        console.error("Error:", error);
       }
-    } else {
-      setSongs([]); // Empty search query
     }
+    return [];
   }
+
+  const debounceSearch = utilService.debounce(async (query, maxResults) => {
+    const searchResult = await searchVideos(query, maxResults);
+    setSongs(searchResult);
+  }, 500);
 
   return (
     <section className="song-search-bar">
@@ -70,11 +69,16 @@ export function SongSearchBar() {
             className="bg-transparent border-none font"
             type="text"
             placeholder="Search for songs"
-            onChange={utilService.debounce(onChange)}
+            value={query}
+            onChange={(ev) => setQuery(ev.target.value)}
           />
         </div>
       </form>
-      <SearchResultsList songs={songs} />
+      {!!songs.length && <SearchResultsList songs={songs} />}
+      {!songs.length && query && <div className="no-results-msg w-100 flex column align-center justify-center">
+        <span className="fs24 font-bold">No results found for {`"${query}"`}</span>
+        <span className="fs14">Please make sure your words are spelled correctly, or use fewer or different keywords.</span>
+      </div> }
     </section>
   );
 }
