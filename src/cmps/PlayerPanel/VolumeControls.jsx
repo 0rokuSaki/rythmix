59556b46d4c3b8deefc1dff4be882784fd101a5d
BAAARON2 <BAAARON2@365.openu.ac.il>
2024-04-12 18:13:09 +0300

    Add volume control interface

--- src/cmps/PlayerPanel/VolumeControls.jsx
+++ src/cmps/PlayerPanel/VolumeControls.jsx
@@ -1,19 +1,38 @@
 import { useState } from "react";
 
+import Slider from "@mui/material/Slider";
+
 import { VolumeIcon } from "../icons/VolumeIcon";
 
 export function VolumeControls() {
-  const [volume, setVolume] = useState(50);
+  const [volume, setVolume] = useState(100);
+  const [prevVolume, setPrevVolume] = useState(volume);
+
+  function onVolumeClick() {
+    if (volume == 0) {
+      setVolume(prevVolume);
+    } else {
+      setPrevVolume(volume);
+      setVolume(0);
+    }
+  }
 
   function handleChange(event) {
     setVolume(event.target.value);
   }
 
   return (
-    <div className="volume-controls flex row align-center justify-center">
-      <button className="volume-btn">
+    <div className="volume-controls flex row align-center justify-end">
+      <button className="volume-btn flex align-center justify-center" onClick={onVolumeClick}>
         <VolumeIcon volume={volume} />
       </button>
+      <Slider
+        className="volume-slider"
+        value={volume}
+        size="medium"
+        onChange={handleChange}
+        aria-labelledby="continuous-slider"
+      />
     </div>
   );
 }
